687ffad84dd1f0c2e8da1eb69e993455d950d98d
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-21 18:06:38 +0700

    Добавлена моделька TokenResponseModel

---
+++ MISBack/MISBack/Data/Models/TokenResponseModel.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MISBack.Data.Models;
+
+public class TokenResponseModel
+{
+    [Required]
+    [MinLength(1)]
+    public string Token { get; set; }
+}
