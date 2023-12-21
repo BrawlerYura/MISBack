c85e9cdabb76e6cdb6c190eb4a96774d60755574
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-21 17:35:57 +0700

    Добавлена моделька InspectionEditModel

---
+++ MISBack/MISBack/Data/Models/InspectionEditModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MISBack.Data.Models;
+
+public class InspectionEditModel
+{
+    [MaxLength(5000)]
+    public string? Anamnesis {  get; set; }
+    
+    [Required]
+    [MaxLength(5000)]
+    [MinLength(1)]
+    public string Complaints { get; set; }
+    
+    [Required]
+    [MaxLength(5000)]
+    [MinLength(1)]
+    public string Treatment { get; set; }
+    
+    [Required]
+    public Conclusion Conclusion { get; set; }
+    
+    public DateTime? NextVisitDate { get; set; }
+    
+    public DateTime? DeathDate { get; set; }
+    
+    [Required]
+    public List<DiagnosisCreateModel> Diagnoses { get; set; }
+}
