84e61b12f82e2306cee9592004b76b52ad2dc204
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-21 17:06:27 +0700

    добавлена моделька Icd10SearchModel

---
+++ MISBack/MISBack/Data/Models/Icd10SearchModel.cs
@@ -0,0 +1,8 @@
+namespace MISBack.Data.Models;
+
+public class Icd10SearchModel
+{
+    public List<Icd10RecordModel>? Records { get; set; }
+    
+    public PageInfoModel? Pagination { get; set; }
+}
