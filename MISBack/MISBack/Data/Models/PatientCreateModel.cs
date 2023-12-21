e18c44cfadc4f968adfe1c54dbc76f6e2b108ebb
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-21 17:50:17 +0700

    Добавлена моделька PatientCreateModel

---
+++ MISBack/MISBack/Data/Models/PatientCreateModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MISBack.Data.Models;
+
+public class PatientCreateModel
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(1000)]
+    public string Name {  get; set; }
+    
+    public DateTime? Birthday { get; set; }
+    
+    [Required]
+    public Gender Gender { get; set; }
+}
