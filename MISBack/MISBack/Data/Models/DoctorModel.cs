1cb383d5539830f6be1c6a042c80b6631dda6c7f
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-21 16:56:11 +0700

    добавлена моделька DoctorModel

---
+++ MISBack/MISBack/Data/Models/DoctorModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MISBack.Data.Models;
+
+public class DoctorModel
+{
+    [Required]
+    public Guid Id { get; set; }
+    
+    [Required]
+    public DateTime CreateTime { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    public string Name { get; set; }
+
+    public DateTime? BirthDate { get; set; }
+    
+    [Required]
+    public Gender Gender { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Phone]
+    public string? Phone { get; set; }
+}
