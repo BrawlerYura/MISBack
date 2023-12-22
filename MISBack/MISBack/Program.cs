8dfc3527eea8cbb18d18160a8ef30173bf0fe08b
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-22 18:14:16 +0700

    исправлена ошибка работы проекта
    добавлен плагин BCrypt

--- MISBack/MISBack/Program.cs
+++ MISBack/MISBack/Program.cs
@@ -6,8 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MISBack.Data;
-using MISBack.Data.Entities;
-using MISBack.Data.Models;
 using MISBack.Services;
 using MISBack.Services.Interfaces;
 
